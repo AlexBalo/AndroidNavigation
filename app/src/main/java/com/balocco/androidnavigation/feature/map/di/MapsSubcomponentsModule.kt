de9131db06b20cef615653be8062d7b146b11dbe
Alessandro Balocco <alex.balo@gmail.com>
2020-08-12 16:18:50 +0200

    Self review

--- app/src/main/java/com/balocco/androidnavigation/feature/map/di/MapsSubcomponentsModule.kt
+++ app/src/main/java/com/balocco/androidnavigation/feature/map/di/MapsSubcomponentsModule.kt
@@ -1,21 +1,6 @@
 package com.balocco.androidnavigation.feature.map.di
 
-import com.balocco.androidnavigation.data.local.UserLocationLocalDataSourceImpl
-import com.balocco.androidnavigation.di.ActivityScope
-import com.google.android.gms.location.FusedLocationProviderClient
 import dagger.Module
-import dagger.Provides
 
 @Module
-class MapsSubcomponentsModule {
-
-    @Provides
-    @ActivityScope
-    fun provideUserLocationProviderClient(
-        fusedLocationProviderClient: FusedLocationProviderClient
-    ): UserLocationLocalDataSourceImpl =
-        UserLocationLocalDataSourceImpl(
-            fusedLocationProviderClient
-        )
-
-}
+class MapsSubcomponentsModule
